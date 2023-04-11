45cd4a499bfec979caf56b77022ee837b8214323
Stasy-Nabieva <nabieva-nasty@mail.ru>
2023-04-11 16:54:33 +0300

    Create HW2.py

---
+++ Python/Простые задачи/HW2.py
@@ -0,0 +1,25 @@
+from random import *
+S1=[]
+N1=randint(50,100)
+for i in range(N1):
+    s1=randint(0,9999)
+    S1.append(s1)
+S1.sort()
+print(*S1)
+
+S2=[]
+N2=randint(50,100)
+for i in range(N2):
+    s2=randint(0,9999)
+    S2.append(s2)
+S2.sort()
+print(*S2)
+
+S3=[]
+for i in range(N1):
+    if S1[i] in S2 : i=i+0
+    else:
+        S3.append(S1[i])
+S3.sort()
+print(*S3)
+        
