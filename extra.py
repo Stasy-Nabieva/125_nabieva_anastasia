ec318958f73704e2d3d9bef9ad785614d35c27be
Stasy-Nabieva <nabieva-nasty@mail.ru>
2023-06-20 10:49:26 +0300

    Create extra.py

---
+++ extra.py
@@ -0,0 +1,12 @@
+string=[True,  True,  True,  False,
+  True,  True,  True,  True,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+
+total = 0
+for i in string:
+            if i is True:
+                        total += i
+print(total)
